b5b67eb406cd0597602f6242c6ca955e65b4ea3b
weite122 <949193531@qq.com>
2018-10-31 00:22:31 +0800

    [A]创建store 和 reducer

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,26 +1,25 @@
 import React, {Component, Fragment} from 'react'
 import 'antd/dist/antd.css'
 import {Input, Button, List} from 'antd'
+import store from './store/index'
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = store.getState()
+  }
+
   render() {
     return (
       <Fragment>
         <div style={{marginTop: "10px", marginLeft: "10px"}}>
-          <Input placeholder='todo Info' style={{width: "300px", marginRight: "10px"}}/>
+          <Input value={this.state.inputValue} placeholder='todo Info' style={{width: "300px", marginRight: "10px"}}/>
           <Button type="primary">Submit</Button>
           <List
             style={{marginTop: "10px", width: "300px"}}
             bordered
-            dataSource={data}
+            dataSource={this.state.list}
             renderItem={item => (<List.Item>{item}</List.Item>)}
           />
         </div>
---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+const defaultstate = {
+  inputValue: 'dd',
+  list: [1, 2]
+}
+
+export default (state = defaultstate, action) => {
+  return state
+}
