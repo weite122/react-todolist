8012d7a131c776551efe48b122b5eaa48c9a1df9
weite122 <949193531@qq.com>
2018-10-31 09:59:02 +0800

    [A]用actionCreator优化action

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react'
 import 'antd/dist/antd.css'
 import {Input, Button, List} from 'antd'
 import store from './store/index'
-
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction} from "./store/actionCreators"
 
 class TodoList extends Component {
   constructor(props) {
@@ -36,26 +36,18 @@ class TodoList extends Component {
     )
   }
   handleInputChange (e) {
-    const action = {
-      type: 'change_input_value',
-      value: e.target.value
-    }
+    const action = getInputChangeAction( e.target.value)
     store.dispatch(action)
   }
   handleStoreChange() {
     this.setState(store.getState())
   }
   handleBtnClick () {
-    const action = {
-      type: 'add_todo_item'
-    }
+    const action = getAddItemAction()
     store.dispatch(action)
   }
   handleItemDelete (index) {
-    const action = {
-      type: 'delete_todo_item',
-      index: ''
-    }
+    const action = getDeleteItemAction(index)
     store.dispatch(action)
   }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './actionTypes'
+
 const defaultstate = {
   inputValue: 'Krive Irving',
   list: [1, 2]
@@ -5,18 +7,18 @@ const defaultstate = {
 // reducer 可以接受state，但不能修改state
 export default (state = defaultstate, action) => {
 
-  if (action.type === 'change_input_value') {
+  if (action.type === CHANGE_INPUT_VALUE) {
     const newState = JSON.parse(JSON.stringify(state))
     newState.inputValue = action.value
     return newState
   }
-  if (action.type === 'add_todo_item') {
+  if (action.type === ADD_TODO_ITEM) {
     const newState = JSON.parse(JSON.stringify(state))
     newState.list.push(newState.inputValue)
     newState.inputValue = ''
     return newState
   }
-  if (action.type === 'delete_todo_item') {
+  if (action.type === DELETE_TODO_ITEM) {
     const newState = JSON.parse(JSON.stringify(state))
     newState.list.splice(action.index, 1)
     return newState
