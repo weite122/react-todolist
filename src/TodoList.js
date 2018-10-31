1c297e9b0c523d0f76b00e46c9c4eb6f0ff128e6
weite122 <949193531@qq.com>
2018-10-31 14:25:34 +0800

    [A]使用easy-mock发送ajax请求

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import store from './store/index'
-import { getInputChangeAction, getAddItemAction, getDeleteItemAction} from "./store/actionCreators"
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction, initListAction } from "./store/actionCreators"
 import TodolistUI from './TodolistUI'
+import axios from 'axios'
 
 class TodoList extends Component {
   constructor(props) {
@@ -41,6 +42,16 @@ class TodoList extends Component {
   handleItemDelete (index) {
     const action = getDeleteItemAction(index)
     store.dispatch(action)
+  }
+
+  componentDidMount() {
+    axios.get('https://easy-mock.com/mock/5bd942f5c33ab80683b9b4c1/reactTodo/list').then((res) => {
+      const data = res.data.Array
+      const action = initListAction(data)
+      store.dispatch(action)
+    }).catch(()=>{
+      console.log('error')
+    })
   }
 }
 
